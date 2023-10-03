c97c5413ea8f759f84a6163e2492699696c2ca97
tanviralam1998 <tanvir.ctg1998@gmail.com>
2023-10-03 07:38:43 -0700

    Full project Done

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -5,5 +5,28 @@ $(document).ready(function () {
             enabled: true
         }
     });
-
+    $('#nav').onePageNav({
+        currentClass: 'active'
+    });
 });
+
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > 20) {
+        $('.header-area').addClass('sticky');
+    } else {
+        $('.header-area').removeClass('sticky');
+    }
+    if ($(this).scrollTop() > 300) {
+        $('#return').addClass('scroll-top');
+    } else {
+        $('#return').removeClass('scroll-top');
+    }
+    $('#return').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    })
+});
+// $(window).on('scroll', function () {
+    
+// })
